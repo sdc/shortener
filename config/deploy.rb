7114ff2fe8b7a2c5f38b10f008f4e146d5af4f52
Paul Vaughan <paulvaughan@southdevon.ac.uk>
2014-07-15 14:22:42 +0100

    Added .htaccess to /shared and edited deploy.rb to symlink it.

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :log_level, :debug
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{user/config.php}
+set :linked_files, %w{user/config.php .htaccess}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
